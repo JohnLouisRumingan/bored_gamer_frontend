1ad64590f51737dd1806c2f78ccbc104357d64f9
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-10 18:09:57 -0400

    Added login and about components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import {Route, Switch, Redirect, withRouter} from 'react-router-dom';
+import NavBar from './Components/Navbar.js'
+import About from './Components/About.js'
+import Login from './Components/Login.js'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{height: '100%'}}>
+      <NavBar />
 
-      
+      <Switch>
+        <Route exact path='/about' component={About}/>
+        <Route exact path='/login' render={()=><Login />} />
+      </Switch>
+  
     </div>
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Login = () => {
+
+    return (
+        <div>
+            Login Form here
+        </div>
+    )
+}
+
+export default Login

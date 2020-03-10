cfa1c6463ff79496a4c9d666c8de080761a9c171
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-10 18:31:28 -0400

    Added redux reducer and store

---
+++ src/redux/reducer.js
@@ -0,0 +1,17 @@
+let initialState = {
+    count: 1
+}
+
+
+
+const reducer = (state = initialState, action) =>{
+    
+    switch(action.type){
+
+        default:
+            return state
+    }
+}
+
+
+export default reducer

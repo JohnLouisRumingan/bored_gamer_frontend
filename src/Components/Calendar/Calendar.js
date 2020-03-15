a31277f3383ef96dffc4408122aa253df814df59
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-14 23:59:15 -0400

    Added calendar week

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -1,18 +1,73 @@
 import React from 'react';
-import dateFns from "date-fns";
+import * as dateFns from "date-fns";
+import './calendar.css'
 
 class Calendar extends React.Component {
 
     state = {
+        currentMonth: new Date(),
+        selectedDate: new Date()
+    }
 
+    renderHeader(){
+        const dateFormat = "mmmm yyyy";
+        return (
+            <div className="header row flex-middle">
+            <div className="col col-start">
+                <div className="icon" onClick={() => this.prevMonth()}>
+                chevron_left
+                </div>
+            </div>
+            <div className="col col-center">
+                <span>
+                {dateFns.format(this.state.currentMonth, dateFormat)}
+                </span>
+            </div>
+            <div className="col col-end" onClick={() => this.nextMonth()}>
+                <div className="icon">chevron_right</div>
+            </div>
+            </div>
+        )
+    }
+
+    renderDays() {
+        const dateFormat = "dddd";
+        const days = [];
+        let startDate = dateFns.startOfWeek(this.state.currentMonth);
+        for (let i = 0; i < 7; i++) {
+            days.push(
+            <div className="col col-center" key={i}>
+                {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+            </div>
+            );
+        }   
+        return <div className="days row">{days}</div>;
+    }
+    
+    renderCells() {}
+
+    onDateClick = (day) => {
+
+    }
+
+    nextMonth = () => {
+        this.setState({
+            currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+          });
+    }
+
+    prevMonth = () => {
+        this.setState({
+            currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+          });
     }
 
     render(){
         return (
-            <div>
-                <div>Header</div>
-                <div>Days</div>
-                <div>Cells</div>
+            <div className='calendar'>
+                {this.renderHeader()}
+                {this.renderDays()}
+                {this.renderCells()}
             </div>
         )
     }

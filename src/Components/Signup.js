96ed931eb33efb8b145d69dfb57d89e71d803afc
John Louis Rumingan <jmrumingan@gmail.com>
2020-03-21 11:26:44 -0400

    Started controlled form

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,16 +1,33 @@
 import React from 'react'
+import { Form } from 'semantic-ui-react'
+import { isThisSecond } from 'date-fns';
 
 
 class Signup extends React.Component {
 
     state={
+        username: "",
 
+    }
+
+    changeHandler = (e, key) => {
+        let formObj = {};
+        formObj[key] = e.target.value;
+        this.setState(formObj)
     }
 
     render(){
 
         return(
-            <div> Signup form here </div>
+            <div> Signup form 
+                <Form>
+                    <Form.Group widths='equal'>
+                        <Form.Input fluid label='Username' placeholder='Enter a username'
+                            value={this.state.username} onChange={(e) => this.changeHandler(e, "username")}
+                        />
+                    </Form.Group>
+                </Form>
+            </div>
         )
     }
 }
